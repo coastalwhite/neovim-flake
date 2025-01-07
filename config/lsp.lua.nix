6bec4f265c095ba30dfd2f8de12cdb47f853098d
coastalwhite <me@gburghoorn.com>
2025-01-07 09:21:14 +0100

    add fix for error

--- config/lsp.lua.nix
+++ config/lsp.lua.nix
@@ -99,6 +99,17 @@ local on_attach = function(client, bufnr)
 	client.server_capabilities.semanticTokensProvider = nil
 end
 
+-- Temporary fix to address https://github.com/neovim/neovim/issues/30985
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities()
 local flags = { debounce_text_changes = 150 }
